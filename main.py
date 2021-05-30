13e549a01d20f65794f439b3dc7ab05b266cd1a7
Тимофей <porno_mail@bk.ru>
2021-05-30 17:43:15 +0400

    size photo changed

--- main.py
+++ main.py
@@ -20,6 +20,51 @@ token_file = 'token.txt'
 
 
 def file_from_vk(owner_id: string):
+    with open(token_file) as gitignore:
+        gitignore.readline().rstrip()
+        TOKEN = gitignore.readline().rstrip()
+        url = 'https://api.vk.com/method'
+        url_photo = url + '/photos.get'
+        url_user = url + '/users.get'
+        params = {
+            'user_ids': owner_id,
+            'access_token': TOKEN,
+            'v': '5.131',
+        }
+        if not owner_id.isdecimal():
+            response = requests.get(url_user, params=params).json()
+            pprint(response)
+            if 'error' in response:
+                print('Ошибка с определением ID - ', response['error']['error_msg'])
+                return
+            owner_id = response['response'][0]['id']
+        params = {
+            'owner_id': owner_id,
+            'access_token': TOKEN,
+            'album_id': 'profile',
+            'v': '5.131',
+            'extended': '1',
+            'photo_sizes': '1'
+        }
+        photos_vk = []
+        response = requests.get(url_photo, params=params)
+        if 'response' in response.json():
+            print('Запрос на скачивание фото из контакта успешен')
+            photo_count = int(input('Сколько фото нужно скачать:\n'))
+            i = 0
+            for items in response.json()['response']['items']:
+                i += 1
+                if i > photo_count:
+                    break
+                print(f'Скачиваем фото {items["id"]}')
+                photos_vk.append(max_photo_size(items))
+            return photos_vk
+        else:
+            print('Ошибка - ', response.json()['error']['error_msg'])
+            return
+
+
+def max_photo_size(item):
     vk_photo_sizes = {
         's': 75,
         'm': 130,
@@ -30,64 +75,22 @@ def file_from_vk(owner_id: string):
         'o': 130,
         'p': 200,
         'r': 510
-        }
-    with open(token_file) as gitignore:
-        gitignore.readline().rstrip()
-        TOKEN = gitignore.readline().rstrip()
-    url = 'https://api.vk.com/method'
-    url_photo = url + '/photos.get'
-    url_user = url + '/users.get'
-    params = {
-        'user_ids': owner_id,
-        'access_token': TOKEN,
-        'v': '5.131',
     }
-    if not owner_id.isdecimal():
-        response = requests.get(url_user, params=params).json()
-        pprint(response)
-        if 'error' in response:
-            print('Ошибка с определением ID - ', response['error']['error_msg'])
-            return
-        owner_id = response['response'][0]['id']
-    params = {
-        'owner_id': owner_id,
-        'access_token': TOKEN,
-        'album_id': 'profile',
-        'v': '5.131',
-        'extended': '1',
-        'photo_sizes': '1'
-    }
-
-    photos_vk = []
-    response = requests.get(url_photo, params=params)
-    if 'response' in response.json():
-        print('Запрос на скачивание фото из контакта успешен')
-        photo_count = int(input('Сколько фото нужно скачать:\n'))
-        i = 0
-        for items in response.json()['response']['items']:
-            i += 1
-            if i > photo_count:
-                break
-            likes = items['likes']['count']
-            print(f'Скачиваем фото {items["id"]}(Кодовое имя {likes})')
-            max_width = 0
-            photo = ''
-            photo_size = ''
-            for sizes in items['sizes']:
-                if vk_photo_sizes[sizes['type']] >= max_width:
-                    max_width = vk_photo_sizes[sizes['type']]
-                    photo = sizes['url']
-                    photo_size = sizes['type']
-            # pprint(photo)
-            if photo:
-                photos_vk.append({'date': items['date'],
-                                  'likes': likes,
-                                  'photo_url': photo,
-                                  'size': photo_size})
-        return photos_vk
-    else:
-        print('Ошибка - ', response.json()['error']['error_msg'])
-        return
+    max_size = 0
+    photo = ''
+    photo_size = ''
+    for sizes in item['sizes']:
+        size = sizes['width'] * sizes['height']
+        if size == 0:
+            size = vk_photo_sizes[sizes['type']]
+        if size >= max_size:
+            max_size = size
+            photo = sizes['url']
+            photo_size = sizes['type']
+    return {'date': item['date'],
+            'likes': item['likes']['count'],
+            'photo_url': photo,
+            'size': photo_size}
 
 
 def file_to_disk(photos_disk: list, OAuth: string):
@@ -95,7 +98,6 @@ def file_to_disk(photos_disk: list, OAuth: string):
         with open(token_file) as gitignore:
             OAuth = gitignore.readline().rstrip()
     resource_url = "https://cloud-api.yandex.net/v1/disk/resources"
-    upload_url = resource_url + '/upload'
     headers = {'Content-Type': 'application/json',
                'Authorization': 'OAuth {}'.format(OAuth)}
     response = requests.put(resource_url, headers=headers, params={'path': 'vk/'})
@@ -104,24 +106,30 @@ def file_to_disk(photos_disk: list, OAuth: string):
         info_json = []
         name_list = []
         for photo in photos_disk:
-            name = str(photo['likes'])
-            if name in name_list:
-                name += str(photo['date'])
+            name = post_file(photo, name_list, headers)
             name_list.append(name)
-            params = {'url': photo['photo_url'],
-                      'path': 'vk/' + name}
-            response = requests.post(upload_url, headers=headers, params=params)
-            if response.status_code == 202:
-                print(f'Файл {name} загружен на ЯДиск')
-                info_json.append({'file_name': name,
-                                  'size': photo['size']})
-            else:
-                print('Ошибка при загрузке фото -', response.json()['message'])
-                return
+            info_json.append({'file_name': name,
+                              'size': photo['size']})
     else:
         print('Ошибка при подключении к ЯДиску -', response.json()['message'])
         return
     return info_json
+
+
+def post_file(photo, name_list, headers):
+    upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+    name = str(photo['likes'])
+    if name in name_list:
+        name += str(photo['date'])
+    params = {'url': photo['photo_url'],
+              'path': 'vk/' + name}
+    response = requests.post(upload_url, headers=headers, params=params)
+    if response.status_code == 202:
+        print(f'Файл {name} загружен на ЯДиск')
+        return name
+    else:
+        print('Ошибка при загрузке фото -', response.json()['message'])
+        return
 
 
 if __name__ == "__main__":
